83d44bc8db838da09bffed6ae7eb3f5d7cd7e773
tpham13 <tpham0123@gmail.com>
2021-03-16 18:31:29 -0700

    Done.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,46 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import { fetchCats } from './actions/catActions'
+import CatList from './CatList.js'
 
 class App extends Component {   
   
+  componentDidMount() {
+    // console.log(this.props)
+    this.props.fetchCats()
+  }
+
+  handleLoading = () => {
+    console.log(this.props.loading)
+    if(this.props.loading) {
+      return <div>Loading...</div>
+    }else {
+      return <CatList catPics={this.props.catPics} />
+    }
+  }
   render() {
+    console.log(this.props.catPics)
     return (
+      
       <div>
         <h1>CatBook</h1>
         {/* add CatList component here */}
+        {this.handleLoading()}
       </div>
     );
   }
 }
 
-export default App
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchCats: () => dispatch(fetchCats())
+  }
+}
+const mapStateToProps = state => {
+  return {
+    catPics: state.cats, 
+    loading: state.loading
+  }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(App)
 
--- src/index.js
+++ src/index.js
@@ -2,8 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 import App from './App'
 
+import {createStore, applyMiddleware, compose} from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk'
+import catsReducer from './reducers/catsReducer.js'
+ 
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(catsReducer, applyMiddleware(thunk))
 
 ReactDOM.render(
-    <App />,
+    <Provider store={store}><App /></Provider>,
     document.getElementById('root')
 );
